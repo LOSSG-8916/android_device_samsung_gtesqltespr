575997ee3ab6774136cfffd0ee9c32c8c61e54be
zeeshan <itexpert120@gmail.com>
2021-01-27 20:16:46 +0500

    lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/gtesqltespr
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_gtesqltespr.mk \
-	$(LOCAL_DIR)/pa_gtesqltespr.mk \
 	$(LOCAL_DIR)/lineage_gtesqltespr.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gtesqltespr-eng \
+    lineage_gtesqltespr-userdebug \
+    lineage_gtesqltespr-user
