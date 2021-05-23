8a57e9d0b89306106fdc62ac41f83097bad13bc5
palmbeach05 <davmcleod87@aol.com>
2021-05-23 17:14:02 -0400

    Remove vendorsetup and add lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,3 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_gtesqltespr.mk \
 	$(LOCAL_DIR)/pa_gtesqltespr.mk \
 	$(LOCAL_DIR)/lineage_gtesqltespr.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gtesqltespr-eng \
+    lineage_gtesqltespr-userdebug \
+    lineage_gtesqltespr-user
